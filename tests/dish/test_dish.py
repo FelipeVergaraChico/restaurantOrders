50476ef1e8165f2fdca12c58c4ec5d1d60724071
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-02-20 14:14:26 -0300

    req 1 e 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,25 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    with pytest.raises(ValueError):
+        Dish("sushi", -1)
+
+    with pytest.raises(TypeError):
+        Dish("sushi", "sushi")
+
+    sushi_ss = Dish("sushi", 10)
+    s2_ss = Dish("sushi", 10)
+    salmoon_ss = Dish("salmão", 15)
+
+    assert sushi_ss.name == "sushi"
+    assert sushi_ss.__repr__() == "Dish('sushi', R$10.00)"
+    assert sushi_ss == s2_ss
+    assert sushi_ss != salmoon_ss
+    assert hash(sushi_ss) == hash(s2_ss)
+    assert hash(sushi_ss) != hash(salmoon_ss)
+    assert sushi_ss.get_restrictions() == set()
+    sushi_ss.add_ingredient_dependency("arroz", 2)
+    assert sushi_ss.get_ingredients() == {"arroz"}
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    sushi = Ingredient("sushi")
+    s2 = Ingredient("sushi")
+    temaki = Ingredient("temaki")
+    salmon = Ingredient("salmão")
+
+    assert sushi.name == "sushi"
+    assert sushi.restrictions == set()
+    assert salmon.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert hash(sushi) == hash(s2)
+    assert hash(sushi) != hash(temaki)
+
+    assert sushi == s2
+    assert sushi != temaki
+
+    assert sushi.__repr__() == "Ingredient(sushi)"
